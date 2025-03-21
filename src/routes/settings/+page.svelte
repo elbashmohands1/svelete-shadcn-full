ded800e5ae1706a1a0a7e364f348eb9f7db6fe18
elbashmohands1 <elbashmohands1@gmail.com>
2025-03-21 19:32:48 +0200

    delete unnesserry files

--- src/routes/settings/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import type { PageData } from "../$types";
-    import SettingsForm from "./settings-form.svelte";
-    let { data }: { data: PageData } = $props();
-  </script>
-   
-  <SettingsForm {data} />
